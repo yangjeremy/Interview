bd41d429f9491edbf13a48c89eb869a23d397b5c
yangjeremy <390076563@qq.com>
2017-10-07 22:35:28 +0800

    aaa

---
+++ vueResumeProject/vue-project/src/components/MyDialog.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="dialogWrapper" v-show="visible">
+    <div class="dialog">
+      <header>
+        {{title}}
+        <span class="close" @click="close">×</span>
+      </header>
+      <main>
+        <slot></slot>
+      </main>
+    </div>
+  </div>
+
+</template>
+
+
+<script>
+  export default{
+    name: 'myDialog',
+    props:['title','visible'],
+    methods:{
+      close(){
+       this.$emit("close")
+      }
+    }
+  }
+</script>
+
+<style scoped lang='scss'>
+.dialogWrapper{
+    position: fixed;
+    background: hsla(0, 0%,0%,0.25);
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+.dialog{
+  background: #fff;
+  min-height: 4em;
+  min-width: 10em;
+    >header{
+      padding: 16px;
+      display: flex;
+      justify-content: space-between;
+
+    }
+    >main{
+      padding: 16px;
+    }
+}
+</style>
---
+++ vueResumeProject/vue-project/src/lib/leancloud.js
@@ -0,0 +1,10 @@
+import AV from 'leancloud-storage'
+
+var APP_ID = 'qe3u4R1BRG4dhe2MSxbs0cwM-gzGzoHsz';
+var APP_KEY = 'u41XjPW3LTeUYwrzyTJ6WxVd';
+ AV.init({
+   appId: APP_ID,
+   appKey: APP_KEY
+ });
+
+ export default AV
